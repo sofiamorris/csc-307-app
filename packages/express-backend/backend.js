5ce7ac692599a80cc7de3f2046e822d99cf19ecb
sofiamorris <sofiamorris213@gmail.com>
2024-09-27 12:00:30 -0700

    post user

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -43,6 +43,11 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
 users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+  
 
 app.get("/users", (req, res) => {
 const name = req.query.name;
@@ -64,6 +69,12 @@ app.get("/users/:id", (req, res) => {
       res.send(result);
     }
   });
+
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
 
 app.listen(port, () => {
   console.log(
